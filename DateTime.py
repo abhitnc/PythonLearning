849f2b74ce619e493ceaf80e3b3157d93eb4d81a
Abhishek Maurya <mumbai3g@gmail.com>
2019-09-08 04:12:52 +0530

    removed comment

--- DateTime.py
+++ DateTime.py
@@ -4,7 +4,6 @@ def main():
     now=datetime.now()
     print(now.strftime("the current date is:%A %d %b %Y"))
 
-    #Printing local system's date and time
     print(now.strftime("locals datetime: %c"))
     print(now.strftime("locals date: %x"))
     print(now.strftime("locals time: %X"))
